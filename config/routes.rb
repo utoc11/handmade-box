5169e048090207fa7b6bc80de1c5d1cb30f9209c
utoc11 <yutoc11@gmail.com>
2018-12-22 22:32:29 +0900

    ユーザー詳細画面の仮作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users, controllers: { :omniauth_callbacks => "omniauth_callbacks" }
   root 'home#index'
-  get 'home/show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users, :only => [:show]
+
 end
